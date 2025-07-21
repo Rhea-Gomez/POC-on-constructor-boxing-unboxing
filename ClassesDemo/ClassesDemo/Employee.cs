d8efadde05aa7ce02f3ac9898291e81487660beb
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-22 00:50:55 +0530

    Constructor & constructor overloading

---
+++ ClassesDemo/ClassesDemo/Employee.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDemo
+{
+    public enum Designations { STAFF, MANAGER, VICEPRESIDENT}
+    internal class Employee
+    {
+        //private attributes
+        private int empId;
+        private string empName;
+        public static double empSalary = 30000;
+        private Designations designation;
+
+        //public properties/getters & setters
+        public int EmpId {  get { return empId; } set { empId = value; } }
+
+        public string EmpName { get { return empName; } set { empName = value; } }
+
+        //defaut constructor
+        public Employee()
+        {
+
+        }
+
+        //parameterized constructor
+        public Employee(int id, string name, Designations designation)
+        {
+            EmpId = id;
+            EmpName = name;
+            this.designation = designation;
+        }
+
+        //copy constructor
+        public Employee(Employee obj)
+        {
+            this.empId = obj.EmpId;
+            this.empName = obj.EmpName;
+            this.designation = obj.designation;
+        }
+
+        //constructor overloading
+        public Employee(int id, string name)
+        {
+            EmpId = id;
+            EmpName = name;
+        }
+
+        public override string ToString()
+        {
+            string value = $"Id: {empId}, Name: {empName}, Salary: {empSalary}, Designation: {designation}";
+            return value;
+        }
+
+        //Instance method to display the Salary Distribution
+        public void SalaryDistribution()
+        {
+            double HRA = empSalary * 0.1;
+            double allowance = 10000;
+            double basicSalary = empSalary - HRA - allowance;
+            Console.WriteLine($"Basic Salary: {basicSalary, 20:F3}, HRA: {HRA, 20:F2}, Allowance: {allowance, 20}");            //string formatters
+            Console.WriteLine("Basic Salary: {0, -20}, HRA: {1, -20}, Allowance: {2, -20}", basicSalary, HRA, allowance);
+        }
+
+        public void SalaryDistribution(Designations designation)
+        {
+            double hra=0.0, allowance = 0, basicSalary;
+            if(designation == Designations.MANAGER)
+            {
+                hra = empSalary * 0.15;
+                allowance = 15000;
+            }
+            else if(designation == Designations.VICEPRESIDENT)
+            {
+               hra = empSalary * 0.2;
+                allowance = 20000;
+            } else
+            {
+                hra = empSalary * 0.1;
+                allowance = 10000;
+            }
+            basicSalary = empSalary - hra - allowance;
+            Console.WriteLine($"Basic Salary: {basicSalary}, HRA: {hra}, Allowance: {allowance}");
+        }
+
+        public static void CheckBonus(Employee obj)
+        {
+            //If the Designation is Designations.MANAGER, Bonus becomes 10000, else if STAFF 5000, if VICEPRESIDENT 15000
+            if(obj.designation == Designations.STAFF)
+            {
+                Console.WriteLine($"Bonus is 5000");
+            }
+            else if(obj.designation == Designations.MANAGER)
+            {
+                Console.WriteLine($"Bonus is 10000");
+            } else if(obj.designation == Designations.VICEPRESIDENT)
+            {
+                Console.WriteLine($"Bonus is 15000");
+            }
+        }
+
+
+
+        
+
+    }
+}
---
+++ ClassesDemo/ClassesDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ClassesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 101;
+            string name = "Rhea";
+            Designations designation = Designations.STAFF;
+
+            //using default constructor
+            Employee employee1 = new Employee();
+            employee1.EmpId = id;
+            employee1.EmpName = name;
+
+            Console.WriteLine("Employee Details: ");
+            Console.WriteLine(employee1.ToString());
+
+            //using parameterized constructor
+            Employee employee2 = new Employee(id, name, designation);
+            Console.WriteLine("Employee details from Parameterized constructor");
+            Console.WriteLine(employee2.ToString());
+
+            //using copy constructor
+            Employee employee3 = new Employee(102, "Sanjana", Designations.MANAGER);
+            Employee employee4 = new Employee(employee3);
+            Console.WriteLine("Employee details from copy constructor");
+            Console.WriteLine(employee4.ToString());
+
+            //using constructor overloading
+            Employee employee5 = new Employee(104, "Sarah");
+            Console.WriteLine("Employee details from constructor overloading");
+            Console.WriteLine(employee5.ToString());
+
+            //Salary Distribution
+        //    employee4.SalaryDistribution();
+
+        //    //check Bonus
+        //    Employee.CheckBonus(employee4);
+        //    Employee.CheckBonus(employee2);
+
+        //    //Salary distribution based on designation
+        //    employee4.SalaryDistribution(Designations.MANAGER);
+        //    employee3.SalaryDistribution(Designations.VICEPRESIDENT);
+        }
+    }
+}
