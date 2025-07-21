95dad0dbff0fe2b9c6b62b9f4109469803975db6
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-22 01:01:37 +0530

    Assignment on Boxing Unboxing

---
+++ BoxingUnBoxingDemo/BoxingUnBoxingDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BoxingUnBoxingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //boxing
+            int num = 42;
+            object boxedNum = num;
+
+            //un-boxing
+            int unboxedNum = (int)boxedNum;
+
+            Console.WriteLine("Boxed Integer: " + boxedNum);
+            Console.WriteLine("Unboxed Integer: " + unboxedNum);
+        }
+    }
+}
